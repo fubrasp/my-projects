30962a16cff8e1a4820533ae9e37f9a36b3d2813
BERTRAND Guillaume <guillaume5524@hotmail.fr>
2018-03-04 18:42:38 +0100

    prepare to prod

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var i18n = require('./i18n');
+var compression = require('compression');
+var helmet = require('helmet');
+
 
 var app = express();
 
+app.use(helmet());
+app.use(compression()); //Compress all routes
 app.use(express.static('public'));
 
 app.set('views', __dirname + '/views');
